770fc4f630c73845378b281041cba9635046441e
Glacy <glacy.kelly3@gmail.com>
2022-03-07 19:34:06 -0300

    adicionando teste delete para o component heroes

--- src/tests/heroes.component.spec.ts
+++ src/tests/heroes.component.spec.ts
@@ -1,22 +1,28 @@
+import { of } from 'rxjs';
 import { HeroesComponent } from './../app/heroes/heroes.component';
 
 describe('Heroes Component', () => {
 
     let component: HeroesComponent;
     let HEROES;
+    let mockHeroService;
 
-    beforeEach(() =>{
+    beforeEach(() => {
         HEROES = [
             {id: 1, name: 'SpiderDude', strength: 8},
-            {id: 2, name: 'SpiderGuy', strength: 9}
+            {id: 2, name: 'SpiderGuy', strength: 9},
+            {id: 3, name: 'SpiderFellow', strength: 10}
         ]
-        const mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero'])
+
+        mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero'])
+
         component = new HeroesComponent(mockHeroService);
     })
-
+    
     describe('delete', () =>{
 
         it('should remove the indicated hero from the heroes list', () => {
+            mockHeroService.deleteHero.and.returnValue(of(true));
             component.heroes = HEROES;
 
             component.delete(HEROES[2]);
