5636c2e4310bbdff41e0d0113abb1df7d2153357
Glacy <glacy.kelly3@gmail.com>
2022-03-19 11:12:32 -0300

    testando HTML renderizado

--- src/tests/hero.component.shallow.spec.ts
+++ src/tests/hero.component.shallow.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing"
+import { NO_ERRORS_SCHEMA } from '@angular/core'
 import { HeroComponent } from "src/app/hero/hero.component";
+import { By } from "@angular/platform-browser";
 
 describe('HeroComponent (shallow)', () => {
     let fixture: ComponentFixture<HeroComponent>
@@ -7,6 +9,7 @@ describe('HeroComponent (shallow)', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
             declarations: [HeroComponent],
+            schemas: [NO_ERRORS_SCHEMA]
         });
 
         fixture = TestBed.createComponent(HeroComponent);
@@ -16,5 +19,15 @@ describe('HeroComponent (shallow)', () => {
         fixture.componentInstance.hero = {id:1, name: "SuperMan", strength: 3};
 
         expect(fixture.componentInstance.hero.name).toEqual("SuperMan")
+    })
+
+    it('should have the correct hero', () => {
+        fixture.componentInstance.hero = {id:1, name: "SuperMan", strength: 3};
+        fixture.detectChanges();
+
+        // let deA = fixture.debugElement.query(By.css('a'));
+        // expect(deA.nativeElement.textContent).toContain("SuperMan");
+
+        expect(fixture.nativeElement.querySelector('a').textContent).toContain("SuperMan");
     })
 })
