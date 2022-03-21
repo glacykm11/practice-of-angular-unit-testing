3e95fe2c80c879f555d1e19e1937942a1270a41c
Glacy <glacy.kelly3@gmail.com>
2022-03-21 20:24:37 -0300

    adicionando deep tests

---
+++ src/tests/heroes.component.deep.spec.ts
@@ -0,0 +1,53 @@
+import { Component, Input, NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { of } from 'rxjs';
+import { Hero } from 'src/app/hero';
+import { HeroService } from 'src/app/hero.service';
+import { HeroComponent } from 'src/app/hero/hero.component';
+import { HeroesComponent } from '../app/heroes/heroes.component';
+
+describe('HeroesComponent (deep tests)', () => {
+    let component: ComponentFixture<HeroesComponent>;
+    let HEROES;
+    let mockHeroService;
+    let fixture: any;
+
+    beforeEach(() => {
+        mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero'])
+
+        TestBed.configureTestingModule({
+            declarations:[HeroesComponent, HeroComponent],
+            providers: [
+                {provide: HeroService, useValue: mockHeroService}
+            ],
+            schemas: [NO_ERRORS_SCHEMA]
+        })
+
+        HEROES = [
+            {id: 1, name: 'SpiderDude', strength: 8},
+            {id: 2, name: 'SpiderGuy', strength: 9},
+            {id: 3, name: 'SpiderFellow', strength: 10}
+        ]
+
+        fixture = TestBed.createComponent(HeroesComponent);
+    });
+
+    it('should render each hero as a HeroComponent', () => {
+        mockHeroService.getHeroes.and.returnValue(of(HEROES));
+
+        //run ngOnInit()
+        fixture.detectChanges();
+        const heroComponentDEs = fixture.debugElement.queryAll(By.directive(HeroComponent));
+
+        expect(heroComponentDEs.length).toEqual(3);
+
+        for(let i=0; i < heroComponentDEs.length; i++){
+            expect(heroComponentDEs[i].componentInstance.hero).toEqual(HEROES[i]);
+        }
+
+        // expect(heroComponentDEs[0].componentInstance.hero.name).toEqual('SpiderDude');
+        // expect(heroComponentDEs[1].componentInstance.hero.name).toEqual('SpiderGuy');
+        // expect(heroComponentDEs[2].componentInstance.hero.name).toEqual('SpiderFellow');
+    })
+})
--- src/tests/heroes.component.shallow.spec.ts
+++ src/tests/heroes.component.shallow.spec.ts
@@ -1,5 +1,6 @@
 import { Component, Input, NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { of } from 'rxjs';
 import { Hero } from 'src/app/hero';
 import { HeroService } from 'src/app/hero.service';
@@ -61,5 +62,12 @@ describe('Heroes Component (shallow tests)', () => {
         // expect(deA.nativeElement.textContent).toContain("SuperMan");
 
         expect(fixture.nativeElement.querySelector('a').textContent).toContain("SuperMan");
+    })
+
+    it('should create on li for each hero', ()=> {
+        mockHeroService.getHeroes.and.returnValue(of(HEROES))
+        fixture.detectChanges();
+        
+        expect(fixture.debugElement.queryAll(By.css('li')).length).toBe(3)
     })
 })
