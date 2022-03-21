d5049f478a865b429f7982d76aedbe262f241b8d
Glacy <glacy.kelly3@gmail.com>
2022-03-21 20:47:02 -0300

    implementando teste do método getHero()

--- src/tests/hero.service.spec.ts
+++ src/tests/hero.service.spec.ts
@@ -6,6 +6,7 @@ import { HttpClientTestingModule, HttpTestingController } from '@angular/common/
 describe('HeroService', () => {
     let mockMessageService;
     let httpTestingController: HttpTestingController;
+    let service: HeroService;
 
     beforeEach(() => {
         mockMessageService = jasmine.createSpyObj(['add']);
@@ -20,5 +21,25 @@ describe('HeroService', () => {
 
         httpTestingController = TestBed.inject(HttpTestingController);
         let msgSvc = TestBed.inject(MessageService);
+        service = TestBed.inject(HeroService);
+    })
+
+    describe('getHero', () => {
+        
+        it('should call get with the correct URL', () => {
+            //chama o método getHero
+            service.getHero(4).subscribe();
+
+            //testa se a URL está correta
+            const req = httpTestingController.expectOne('api/heroes/4');
+
+            //envia a resposta 
+            req.flush({id:4, name: 'SuperDude', strength: 100})
+
+            //garante que não haja mais de uma chamada
+            httpTestingController.verify();
+
+            expect(req.request.method).toBe('GET');
+        })
     })
 })
